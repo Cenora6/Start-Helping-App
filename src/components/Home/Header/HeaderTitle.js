dd6352635ed42d2221adfb072073f6285f497491
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-01-12 11:58:55 +0100

    Navigation, header (mobile) finished

--- src/components/Home/Header/HeaderTitle.js
+++ src/components/Home/Header/HeaderTitle.js
@@ -3,6 +3,21 @@ import decoration from '../../../assets/Decoration.svg'
 import {Link} from 'react-router-dom';
 
 class HeaderTitle extends Component {
+    state = {
+        width: window.innerWidth,
+    };
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
 
     render() {
 
@@ -12,16 +27,36 @@ class HeaderTitle extends Component {
             color: "#3C3C3C",
         };
 
-        return (
-            <div className='headerTitle' id='start'>
-                <div className='title'>Zacznij pomagać!<br/> Oddaj niechciane rzeczy w zaufane ręce</div>
-                <img src={decoration} alt='decoration'/>
-                <div className='headerButtons'>
-                    <Link to='/logowanie' style={buttonStyle} className='hoverLink'> <p>Oddaj<br/>rzeczy</p></Link>
-                    <Link to='/logowanie' style={buttonStyle} className='hoverLink'><p> Zorganizuj zbiórkę</p></Link>
+        const { width } = this.state;
+        const isMobile = width <= 767;
+
+        if (isMobile) {
+
+            return (
+                <div className='headerMobile' id='start'>
+                    <div className='mobileSubtitle'>Masz w domu rzeczy, z którymi nie wiesz co zrobić?</div>
+                    <div className='mobileTitle'>ODDAJ JE POTRZEBUJĄCYM <br/> - szybko i w zaufane ręce</div>
+                    <div className='headerButtons'>
+                        <Link to='/rejestracja' style={buttonStyle} className='hoverLink'>
+                            <p>Załóż konto</p>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+
+        } else {
+
+            return (
+                <div className='headerTitle' id='start'>
+                    <div className='title'>Zacznij pomagać!<br/> Oddaj niechciane rzeczy w zaufane ręce</div>
+                    <img src={decoration} alt='decoration'/>
+                    <div className='headerButtons'>
+                        <Link to='/logowanie' style={buttonStyle} className='hoverLink'> <p>Oddaj<br/>rzeczy</p></Link>
+                        <Link to='/logowanie' style={buttonStyle} className='hoverLink'><p> Zorganizuj zbiórkę</p></Link>
+                    </div>
+                </div>
+            )
+        }
     }
 }
 
