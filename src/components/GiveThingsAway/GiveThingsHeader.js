36f2c86cc3f908e54f87216722afaec91fc2e1e1
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-01-12 19:16:05 +0100

    Give Things Away Steps (mobile), header + contact finished, steps to fix

--- src/components/GiveThingsAway/GiveThingsHeader.js
+++ src/components/GiveThingsAway/GiveThingsHeader.js
@@ -4,48 +4,82 @@ import Navigation from "../Home/Header/Navigation";
 import decoration from "./../../assets/Decoration.svg"
 
 class GiveThingsAwayHeader extends Component {
+    state = {
+        width: window.innerWidth,
+    };
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
 
     render() {
 
-        return (
-            <section className='giveThingsAway'>
-                <div className='giveThingsAwayHeader'>
-                    <LogReg/>
-                    <Navigation/>
-                </div>
-                <div className='giveThingsAwayWelcome'>
-                    <div className='giveThingsAwayText'>
-                        <h2>Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM</h2>
-                        <img src={decoration} alt={decoration}/>
-                        <h3>Wystarczą 4 proste kroki:</h3>
+        const { width } = this.state;
+        const isMobile = width <= 767;
 
-                        <ul className='giveThingsAwaySteps'>
-                            <li>
-                                <div className='square'></div>
-                                <span>1</span>
-                                <p>Wybierz<br/>rzeczy</p>
-                            </li>
-                            <li>
-                                <div className='square'></div>
-                                <span>2</span>
-                                <p>Spakuj je<br/>w worki</p>
-                            </li>
-                            <li>
-                                <div className='square'></div>
-                                <span>3</span>
-                                <p>Wybierz<br/>fundację</p>
-                            </li>
-                            <li>
-                                <div className='square'></div>
-                                <span>4</span>
-                                <p>Zamów<br/>kuriera</p>
-                            </li>
-                        </ul>
-
+        if (isMobile) {
+            return (
+                <section className='giveThingsAway'>
+                    <div className='giveThingsAwayHeader'>
+                        <Navigation/>
                     </div>
-                </div>
-            </section>
-        )
+                    <div className='giveThingsAwayWelcome'>
+                        <div className='giveThingsAwayText'>
+                            <h2>Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM</h2>
+                            <h3>Wystarczą 4 proste kroki:</h3>
+                        </div>
+                    </div>
+                </section>
+            )
+        } else {
+            return (
+                <section className='giveThingsAway'>
+                    <div className='giveThingsAwayHeader'>
+                        <LogReg/>
+                        <Navigation/>
+                    </div>
+                    <div className='giveThingsAwayWelcome'>
+                        <div className='giveThingsAwayText'>
+                            <h2>Oddaj rzeczy, których już nie chcesz POTRZEBUJĄCYM</h2>
+                            <img src={decoration} alt={decoration}/>
+                            <h3>Wystarczą 4 proste kroki:</h3>
+
+                            <ul className='giveThingsAwaySteps'>
+                                <li>
+                                    <div className='square'></div>
+                                    <span>1</span>
+                                    <p>Wybierz<br/>rzeczy</p>
+                                </li>
+                                <li>
+                                    <div className='square'></div>
+                                    <span>2</span>
+                                    <p>Spakuj je<br/>w worki</p>
+                                </li>
+                                <li>
+                                    <div className='square'></div>
+                                    <span>3</span>
+                                    <p>Wybierz<br/>fundację</p>
+                                </li>
+                                <li>
+                                    <div className='square'></div>
+                                    <span>4</span>
+                                    <p>Zamów<br/>kuriera</p>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </section>
+            )
+        }
     }
 }
 
