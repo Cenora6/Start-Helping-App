4a13be4c5898d18bfe0cf22582e54af0e30c7c46
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-01-14 11:13:32 +0100

    Give Things Away (mobile), Step 5, Summary finished

--- src/components/GiveThingsAway/Step5.js
+++ src/components/GiveThingsAway/Step5.js
@@ -1,76 +1,172 @@
 import React, {Component} from 'react';
 import shirt from './../../assets/Icon-1.svg'
 import arrows from './../../assets/Icon-4.svg'
+import lastStep from "../../assets/Icon-7.svg";
 
 class Step5 extends Component {
+    state = {
+        width: window.innerWidth,
+    };
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
 
     render() {
 
-        return (
-            <>
-                <section className='steps'>
-                    <span className='choose'>Podsumowanie Twojej darowizny</span>
+        const {width} = this.state;
+        const isMobile = width <= 767;
 
-                    <div className='whatToGiveSummary'>
-                        <h2>Oddajesz:</h2>
-                        <div className='giveThingsAndWhere'>
-                            <img src={shirt} alt={shirt}/>
-                            <span>{this.props.selectValue} worki, {this.props.radioValue}, {this.props.checkboxValues.join(', ')}</span>
-                        </div>
-                        <div className='giveThingsAndWhere'>
-                            <img src={arrows} alt={arrows}/>
-                            <span>dla lokalizacji: {this.props.selectedCityOrOrganization}</span>
-                        </div>
+        if (isMobile) {
+            return (
+                <>
+                    <div className='progressBar'>
+                        <span className='bar doneBar'>1</span>
+                        <span className='bar doneBar'>2</span>
+                        <span className='bar doneBar'>3</span>
+                        <span className='bar doneBar'>4</span>
+                        <span className='bar activeBar'><img src={lastStep} alt={lastStep}/></span>
                     </div>
 
-                    <div className='addressDate'>
+                    <div className='titleStep'>
+                        <span className='mobilechoose'>Podsumowanie Twojej darowizny</span>
+                    </div>
 
-                        <div className='summaryAddress'>
-                            <span className='title'>Adres odbioru:</span>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Ulica</span>
-                                <span>{this.props.street}</span>
+                    <section className='steps summaryStep'>
+                        <div className='whatToGiveSummary'>
+                            <h2>Oddajesz:</h2>
+                            <div className='giveThingsAndWhere'>
+                                <img src={shirt} alt={shirt}/>
+                                <span>{this.props.selectValue} worki, {this.props.radioValue}, {this.props.checkboxValues.join(', ')}</span>
                             </div>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Miasto</span>
-                                <span>{this.props.city}</span>
-                            </div>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Kod pocztowy</span>
-                                <span>{this.props.zipcode}</span>
-                            </div>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Numer telefonu</span>
-                                <span>{this.props.phone}</span>
+                            <div className='giveThingsAndWhere'>
+                                <img src={arrows} alt={arrows}/>
+                                <span>dla lokalizacji: {this.props.selectedCityOrOrganization}</span>
                             </div>
                         </div>
 
-                        <div className='summaryDate'>
-                            <span className='title'>Termin odbioru:</span>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Termin odbioru</span>
-                                <span>{this.props.date}9</span>
+                        <div className='addressDate'>
+                            <div className='summaryAddress'>
+                                <span className='title'>Adres odbioru:</span>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Ulica</span>
+                                    <span>{this.props.street}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Miasto</span>
+                                    <span>{this.props.city}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Kod pocztowy</span>
+                                    <span>{this.props.zipcode}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Numer telefonu</span>
+                                    <span>{this.props.phone}</span>
+                                </div>
                             </div>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Godzina</span>
-                                <span>{this.props.time}</span>
+
+                            <div className='summaryDate'>
+                                <span className='title'>Termin odbioru:</span>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Termin odbioru</span>
+                                    <span>{this.props.date}9</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Godzina</span>
+                                    <span>{this.props.time}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Uwagi do kuriera</span>
+                                    <span>{this.props.notes}</span>
+                                </div>
                             </div>
-                            <div className='summaryInput'>
-                                <span className='inputTitle'>Uwagi do kuriera</span>
-                                <span>{this.props.notes}</span>
+
+                        </div>
+
+                        <div className='nextPrevButtons'>
+                            <span className='prevButton hoverLink' onClick={this.props.handlePrevious}>Cofnij</span>
+                            <span className='nextButton hoverLink' onClick={this.props.handleConfirm}>Potwierdzam</span>
+                        </div>
+
+                    </section>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <section className='steps'>
+                        <span className='choose'>Podsumowanie Twojej darowizny</span>
+
+                        <div className='whatToGiveSummary'>
+                            <h2>Oddajesz:</h2>
+                            <div className='giveThingsAndWhere'>
+                                <img src={shirt} alt={shirt}/>
+                                <span>{this.props.selectValue} worki, {this.props.radioValue}, {this.props.checkboxValues.join(', ')}</span>
+                            </div>
+                            <div className='giveThingsAndWhere'>
+                                <img src={arrows} alt={arrows}/>
+                                <span>dla lokalizacji: {this.props.selectedCityOrOrganization}</span>
                             </div>
                         </div>
 
-                    </div>
+                        <div className='addressDate'>
 
-                    <div className='nextPrevButtons'>
-                        <span className='prevButton hoverLink' onClick={this.props.handlePrevious}>Cofnij</span>
-                        <span className='nextButton hoverLink' onClick={this.props.handleConfirm}>Potwierdzam</span>
-                    </div>
+                            <div className='summaryAddress'>
+                                <span className='title'>Adres odbioru:</span>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Ulica</span>
+                                    <span>{this.props.street}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Miasto</span>
+                                    <span>{this.props.city}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Kod pocztowy</span>
+                                    <span>{this.props.zipcode}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Numer telefonu</span>
+                                    <span>{this.props.phone}</span>
+                                </div>
+                            </div>
 
-                </section>
-            </>
-        )
+                            <div className='summaryDate'>
+                                <span className='title'>Termin odbioru:</span>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Termin odbioru</span>
+                                    <span>{this.props.date}9</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Godzina</span>
+                                    <span>{this.props.time}</span>
+                                </div>
+                                <div className='summaryInput'>
+                                    <span className='inputTitle'>Uwagi do kuriera</span>
+                                    <span>{this.props.notes}</span>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className='nextPrevButtons'>
+                            <span className='prevButton hoverLink' onClick={this.props.handlePrevious}>Cofnij</span>
+                            <span className='nextButton hoverLink' onClick={this.props.handleConfirm}>Potwierdzam</span>
+                        </div>
+
+                    </section>
+                </>
+            )
+        }
     }
 }
 
