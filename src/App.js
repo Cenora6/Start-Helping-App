ea6cb97318b81c1dcc638c0274ade959abaf27b8
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-01-19 12:58:42 +0100

    Added authorization and privateRoute

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ import Register from "./components/Register/Register";
 import LogOut from "./components/LogOut/LogOut";
 import GiveThingsAwayMain from "./components/GiveThingsAway/GiveThingsAwayMain";
 import AdminPanel from "./components/Admin/Admin"
+import PrivateRoute from "./components/Session/privateRoute";
+import { AuthProvider } from "./components/Session/auth";
 
 class App extends Component {
 
     render() {
         return (
-            <HashRouter>
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/logowanie' component={Login}/>
-                    <Route path='/rejestracja' component={Register}/>
-                    <Route path='/wylogowano' component={LogOut}/>
-                    <Route path='/oddaj-rzeczy' component={GiveThingsAwayMain}/>
-                    <Route path='/admin' component={AdminPanel}/>
-                </Switch>
-            </HashRouter>
+            <AuthProvider>
+                <HashRouter>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route path='/logowanie' component={Login}/>
+                        <Route path='/rejestracja' component={Register}/>
+                        <Route path='/wylogowano' component={LogOut}/>
+                        <PrivateRoute path='/oddaj-rzeczy' component={GiveThingsAwayMain}/>
+                        <PrivateRoute path='/admin' component={AdminPanel}/>
+                    </Switch>
+                </HashRouter>
+            </AuthProvider>
         );
     }
 }
---
+++ src/components/Session/privateRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+    const currentUser = sessionStorage.getItem("email");
+    return (
+        <Route
+            {...rest}
+            render={routeProps =>
+                (currentUser) ? (
+                    <RouteComponent {...routeProps} />
+                ) : (
+                    <Redirect to={"/logowanie"} />
+                )
+            }
+        />
+    );
+};
+
+export default PrivateRoute
